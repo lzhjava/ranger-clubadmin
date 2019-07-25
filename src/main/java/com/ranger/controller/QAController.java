724b42efdc5c3fcfa01865c464e71645807a3700
sunshuaidong <Sun1170909625@gmail.com>
2019-07-25 14:48:51 +0800

    问答,统计

---
+++ src/main/java/com/ranger/controller/QAController.java
@@ -0,0 +1,35 @@
+package com.ranger.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.ranger.question.contract.QuestionsAndAnswersContract;
+import com.ranger.question.vo.ResultVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述:问答
+ *
+ * @author: sunshuaidong
+ * @create: 2018-12-06 18:08
+ * @brief:
+ */
+@RestController
+@RequestMapping("/questions")
+public class QAController {
+
+    @Reference(interfaceClass = QuestionsAndAnswersContract.class, timeout = 1200000)
+    private QuestionsAndAnswersContract QAContract;
+
+    /**
+     * 俱乐部问答获取可展示的问答列表
+     * @return
+     */
+    @GetMapping("")
+    public ResultVO selectAllQuestions(){
+        return QAContract.selectAllQuestions();
+    }
+
+
+
+}
