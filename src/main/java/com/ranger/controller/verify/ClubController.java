b381a5ed05e814e0dc33d58eea014ff627708fd9
sunshuaidong <Sun1170909625@gmail.com>
2019-09-24 17:52:44 +0800

    修改包引用

--- src/main/java/com/ranger/controller/verify/ClubController.java
+++ src/main/java/com/ranger/controller/verify/ClubController.java
@@ -9,7 +9,9 @@ import com.ranger.club.po.ClubPO;
 import com.ranger.club.vo.ResultVO;
 import org.springframework.web.bind.annotation.*;
 
-/**   俱乐部管理
+/**
+ * 俱乐部管理
+ *
  * @author huipeng
  * @Title: ClubController
  * @ProjectName ranger-clubadmin
@@ -29,6 +31,7 @@ public class ClubController {
 
     /**
      * 查询俱乐部管理员  或者   俱乐部会员（默认查询会员）
+     *
      * @param clubId   俱乐部id
      * @param type     俱乐部身份
      * @param pageNo   当前页码
@@ -39,76 +42,80 @@ public class ClubController {
     public ResultVO searchClubMember(@PathVariable Long clubId,
                                      @RequestParam ClubMemberType type,
                                      @RequestParam(required = false, defaultValue = "0") Integer pageNo,
-                                     @RequestParam(required = false, defaultValue = "10")Integer pageSize,
-                                     String nickName){
-       return clubContract.searchClubManageOrMember(clubId, type, pageNo, pageSize,nickName);
+                                     @RequestParam(required = false, defaultValue = "10") Integer pageSize,
+                                     String nickName) {
+        return clubContract.searchClubManageOrMember(clubId, type, pageNo, pageSize, nickName);
     }
 
 
-
-    /**      后台查看加入俱乐部待审核用户 (ClubId有值查询聚体俱乐部，无值查询全部俱乐部)
-     * @param clubId    俱乐部ID
-     * @param clubJoinVerifyType     加入俱乐部审核状态
-     * @param pageNo    当前页码
-     * @param pageSize     展示条数
+    /**
+     * 后台查看加入俱乐部待审核用户 (ClubId有值查询聚体俱乐部，无值查询全部俱乐部)
+     *
+     * @param clubId             俱乐部ID
+     * @param clubJoinVerifyType 加入俱乐部审核状态
+     * @param pageNo             当前页码
+     * @param pageSize           展示条数
      * @return
      */
     @GetMapping("/{clubId}/join")
     public ResultVO clubJoin(@PathVariable Long clubId,
-                             @RequestParam ClubJoinVerifyType clubJoinVerifyType ,
+                             @RequestParam ClubJoinVerifyType clubJoinVerifyType,
                              @RequestParam(required = false, defaultValue = "0") Integer pageNo,
-                             @RequestParam(required = false, defaultValue = "10")Integer pageSize){
+                             @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
         return clubManageContract.clubJoinCheck(clubId, clubJoinVerifyType, pageNo, pageSize);
     }
 
 
-
-
-    /**   管理后台审核俱乐部会员加入
-     * @param clubId   俱乐部id
-     * @param userId    用户id
-     * @param clubJoinVerifyType    审批状态
+    /**
+     * 管理后台审核俱乐部会员加入
+     *
+     * @param clubId             俱乐部id
+     * @param userId             用户id
+     * @param clubJoinVerifyType 审批状态
      * @return
      */
     @PostMapping("/{clubId}/verify")
     public ResultVO clubMemberVerify(@PathVariable Long clubId,
                                      @RequestParam Long userId,
-                                     @RequestParam ClubJoinVerifyType clubJoinVerifyType){
+                                     @RequestParam ClubJoinVerifyType clubJoinVerifyType) {
         return clubManageContract.clubMemberReview(clubId, userId, clubJoinVerifyType);
     }
 
 
     /**
      * 退出俱乐部
+     *
      * @param clubId 俱乐部ID
      * @param userId 用户ID
      * @return
      */
     @PostMapping("/{clubId}/quit/{userId}")
-    public ResultVO quitClub(@PathVariable Long clubId, @PathVariable Long userId){
-       return clubContract.quitClub(clubId, userId);
+    public ResultVO quitClub(@PathVariable Long clubId, @PathVariable Long userId) {
+        return clubContract.quitClub(clubId, userId);
     }
 
 
-
-
-    /**  俱乐部详情
-     * @param clubId  俱乐部ID
+    /**
+     * 俱乐部详情
+     *
+     * @param clubId 俱乐部ID
      * @return
      */
     @GetMapping("/{clubId}")
     public ResultVO clubInfo(@PathVariable Long clubId) {
-        return  clubContract.clubInfo(clubId,null);
+        return clubContract.clubInfo(clubId, null);
     }
 
 
-    /**   修改俱乐部信息
-     * @param clubId   俱乐部id
-     * @param clubPO   后台俱乐部信息维护po
+    /**
+     * 修改俱乐部信息
+     *
+     * @param clubId 俱乐部id
+     * @param clubPO 后台俱乐部信息维护po
      * @return
      */
     @PutMapping("/{clubId}")
-    public ResultVO modifyClub(@PathVariable Long clubId, @RequestBody ClubPO clubPO){
+    public ResultVO modifyClub(@PathVariable Long clubId, @RequestBody ClubPO clubPO) {
         clubPO.setClubId(clubId);
         return clubManageContract.modifyClub(clubId, clubPO);
     }
