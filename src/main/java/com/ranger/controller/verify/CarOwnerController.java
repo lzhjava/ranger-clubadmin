83280e36f3352ce496ed39193fae9f93573c2d8e
wuhuipeng <wuhuipeng@acegear.com>
2019-11-29 11:18:10 +0800

    车主认证接口部署

---
+++ src/main/java/com/ranger/controller/verify/CarOwnerController.java
@@ -0,0 +1,76 @@
+package com.ranger.controller.verify;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.ranger.club.contract.CarOwnerContract;
+import com.ranger.club.contract.ClubContract;
+import com.ranger.club.contract.ClubManageContract;
+import com.ranger.club.enums.ClubJoinVerifyType;
+import com.ranger.club.enums.ClubMemberType;
+import com.ranger.club.po.ClubOwnerJoinVerifyPO;
+import com.ranger.club.po.ClubOwnerPO;
+import com.ranger.club.po.ClubPO;
+import com.ranger.club.vo.ResultVO;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 车主认证信息管理
+ *
+ * @author huipeng
+ * @Title: ClubController
+ * @ProjectName ranger-clubadmin
+ * @Description: TODO
+ * @date 2019/7/25下午2:42
+ */
+@RestController
+@RequestMapping("/verify/car")
+public class CarOwnerController {
+
+
+    @Reference(interfaceClass = CarOwnerContract.class, timeout = 1200000)
+    private CarOwnerContract carOwnerContract;
+
+
+    @PostMapping("")
+    public  ResultVO saveCarOwner(@RequestBody ClubOwnerPO clubOwnerPO) {
+        return carOwnerContract.saveCarOwner(clubOwnerPO);
+    }   //存储俱乐部车主信息
+
+
+
+    @DeleteMapping("/{id}")
+    public  ResultVO delCarOwner(@PathVariable Long id){
+        return carOwnerContract.delCarOwner(id);
+    }//删除车主信息
+
+
+    @PutMapping("/{id}")
+    public ResultVO modifyCarOwner(@PathVariable Long id,
+                                   @RequestBody ClubOwnerPO clubOwnerPO){
+        return carOwnerContract.modifyCarOwner(id, clubOwnerPO);
+    } //修改车主信息
+
+
+    @GetMapping("{clubId}")
+    public ResultVO searchCarOwners(@PathVariable Long clubId){
+        return carOwnerContract.searchCarOwners(clubId);
+    } //查询俱乐部车主信息列表
+
+
+    @GetMapping("/auditPage")
+    public  ResultVO clubOwnerJoinVerifPage (@RequestParam(required = false, defaultValue = "0") Integer pageNo,
+                                             @RequestParam(required = false, defaultValue = "10") Integer pageSize,
+                                             Long clubId,Integer status){
+        return carOwnerContract.clubOwnerJoinVerifPage(pageNo, pageSize, clubId, status);
+    }        //车主认证审核列表
+
+
+    @PutMapping("/audit/{id}")
+    public  ResultVO reviewTheOwner (@PathVariable  Long id,
+                                     @RequestParam Integer status,
+                                     @RequestParam String content){
+        return carOwnerContract.reviewTheOwner(id, status, content);
+    }      //车主审核
+
+
+
+}
